47200f9e6141875f0b5092632e2de9affaa30129
phppp_1701 <hoangphuonghoctap1701@gmail.com>
2025-03-30 16:43:01 +0700

    Tao push

---
+++ src/gui/Home_GUI.java
@@ -0,0 +1,27 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.geometry.Rectangle2D;
+
+public class Home_GUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Lấy thông tin màn hình chính
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        // In ra kích thước màn hình
+        System.out.println("Width: " + bounds.getWidth());
+        System.out.println("Height: " + bounds.getHeight());
+        System.out.println("Top-Left X: " + bounds.getMinX());
+        System.out.println("Top-Left Y: " + bounds.getMinY());
+
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Khởi chạy JavaFX
+    }
+}
