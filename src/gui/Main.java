c57edfb3b9a12508b89d481fcd6fdd22425231d6
phppp_1701 <hoangphuonghoctap1701@gmail.com>
2025-04-02 21:29:14 +0700

    Hoang Phuong update giao dien dat ve

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,29 +1,15 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        // Lấy kích thước màn hình
-        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
-
-        // Đặt kích thước cửa sổ theo độ phân giải màn hình
-        primaryStage.setWidth(screenBounds.getWidth());
-        primaryStage.setHeight(screenBounds.getHeight());
-
-        // Thiết lập chế độ full màn hình
-        primaryStage.setFullScreen(true);
-        
-        // Tùy chọn: Hiển thị thông báo thoát fullscreen (ấn ESC để thoát)
-        primaryStage.setFullScreenExitHint("");
-        
-        // Hiển thị giao diện quản lý bán vé
+        // Khởi tạo
         new QuanLyBanVe_GUI(primaryStage);
-
+//    	new Home_GUI(primaryStage);
+        
         // Hiển thị cửa sổ
         primaryStage.show();
     }
@@ -31,5 +17,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
 }
