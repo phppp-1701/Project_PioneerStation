2af2779bbd1cd8b0d2beb9aff2d6092663b79225
quangtrung78 <buivantrungxl@gmail.com>
2025-04-03 15:25:28 +0700

    Trung push

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,17 +6,15 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-    	// Tạo và hiển thị màn hình đăng nhập
-//    	new DangNhap_GUI(primaryStage);
-    	//Tạo và hiển thị màn hình chính (Home) thay vì màn hình đăng nhập
-    	new Home_GUI(primaryStage);
-    	
-//    	Tạo và hiển thị màn hình quản lý bán vé
-    	new QuanLyBanVe_GUI(primaryStage);
+        
+        // Hiển thị giao diện quản lý bán vé
+        new ThongKe_GUI(primaryStage);
 
+        // Hiển thị cửa sổ
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch(args); // Khởi chạy JavaFX
+        launch(args);
     }
 }
