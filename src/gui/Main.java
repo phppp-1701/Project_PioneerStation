1769aad6be865f2630ed8000be2e0c237cec80a9
phamvietquaan <quanpham4908@gmail.com>
2025-04-05 23:02:48 +0700

    update lai

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,56 +6,46 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-<<<<<<< HEAD
+	@Override
+	public void start(Stage primaryStage) {
 
-        // Khởi tạo
+		// Khởi tạo
 //      new QuanLyBanVe_GUI(primaryStage);
 //    	new Home_GUI(primaryStage);
 
-    	new ThanhToan_GUI(primaryStage);
-
-
+		// new ThanhToan_GUI(primaryStage);
 
 //    	new QuanLyBanVe_GUI(primaryStage);
 //    	new ThanhToan_GUI(primaryStage);
 
-        
+		// Hiển thị giao diện quản lý bán vé
+		new ThongKe_GUI(primaryStage);
+		// Lấy kích thước màn hình
+		Rectangle2D screenBounds = Screen.getPrimary().getBounds();
 
-        
-        // Hiển thị giao diện quản lý bán vé
-//        new ThongKe_GUI(primaryStage);
-=======
-        // Lấy kích thước màn hình
-        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+		// Đặt kích thước cửa sổ theo độ phân giải màn hình
+		primaryStage.setWidth(screenBounds.getWidth());
+		primaryStage.setHeight(screenBounds.getHeight());
 
-    
-        // Đặt kích thước cửa sổ theo độ phân giải màn hình
-        primaryStage.setWidth(screenBounds.getWidth());
-        primaryStage.setHeight(screenBounds.getHeight());
-        
+		// Đặt kích thước cửa sổ theo độ phân giải màn hình
+		primaryStage.setWidth(screenBounds.getWidth());
+		primaryStage.setHeight(screenBounds.getHeight());
 
-        // Đặt kích thước cửa sổ theo độ phân giải màn hình
-        primaryStage.setWidth(screenBounds.getWidth());
-        primaryStage.setHeight(screenBounds.getHeight());
+		// Thiết lập chế độ full màn hình
+		primaryStage.setFullScreen(true);
 
-        // Thiết lập chế độ full màn hình
-        primaryStage.setFullScreen(true);
-        
-        // Tùy chọn: Hiển thị thông báo thoát fullscreen (ấn ESC để thoát)
-        primaryStage.setFullScreenExitHint("");
+		// Tùy chọn: Hiển thị thông báo thoát fullscreen (ấn ESC để thoát)
+		primaryStage.setFullScreenExitHint("");
 
-        // Hiển thị giao diện quản lý bán vé
-        new QuanLyVe_GUI(primaryStage);
->>>>>>> origin/Tuan
+		// Hiển thị giao diện quản lý bán vé
+		// new QuanLyBanVe_GUI(primaryStage);
 
-        // Hiển thị cửa sổ
-        primaryStage.show();
-    }
+		// Hiển thị cửa sổ
+		primaryStage.show();
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
