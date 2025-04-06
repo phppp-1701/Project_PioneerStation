35ee86223bccf20ce863af76b67983a064c99a21
phppp_1701 <hoangphuonghoctap1701@gmail.com>
2025-04-06 14:28:02 +0700

    Merge code Quan

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-    	new QuanLyTaiKhoan_GUI(primaryStage);
+    	new QuanLyKhachHang_GUI(primaryStage);
         primaryStage.show();
     }
 
